8ce2121d94af89863960ec420c96afac6f1b2829
Mathias Fuchs <mathias@mathiasfuchs.de>
2018-08-09 15:25:19 +0100

    works for one curve!

--- MinSurfaceComponent.cs
+++ MinSurfaceComponent.cs
@@ -1,44 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Grasshopper.Kernel;
 using Rhino.Geometry;
-using MathNet.Numerics;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using Grasshopper.Kernel.Parameters;
-// In order to load the result of this wizard, you will also need to
-// add the output bin/ folder of this project to the list of loaded
-// folder in Grasshopper.
-// You can use the _GrasshopperDeveloperSettings Rhino command for that.
 
 namespace MinSurface
 {
+
+    public class LaplaceData
+    {
+        private double a0;
+        private double[] an;
+        private double[] bn;
+        private int degree;
+
+        // solves the equation Laplace(u) = 0 on the unit disk, 
+        public LaplaceData(List<double> targets, int degree)
+        {
+            this.a0 = targets.Average();
+            this.an = new double[degree + 1];
+            this.bn = new double[degree + 1];
+            this.degree = degree;
+
+            // can obviously be parallelized
+            for (int i = 1; i < degree; i++)
+            {
+                this.an[i] = targets.Select((double v, int ii) => 2.0 * v * Math.Cos((double)(i * ii) / (double)targets.Count * 2 * Math.PI)).Average();
+                this.bn[i] = targets.Select((double v, int ii) => 2.0 * v * Math.Sin((double)(i * ii) / (double)targets.Count * 2 * Math.PI)).Average();
+            }
+        }
+
+        public double eval(Point2d p)
+        {
+            var r = Math.Sqrt(p.X * p.X + p.Y * p.Y);
+            var theta = Math.Atan2(p.Y, p.X);
+            // Horner's rule could speed it up but not bottleneck
+            return this.a0 + Enumerable.Range(1, this.degree).Sum(i => Math.Pow(r, i) * (this.an[i] * Math.Cos(i * theta) + this.bn[i] * Math.Sin(i * theta)));
+        }
+    }
+
+    public class AnnularLaplaceData
+    {
+        private double a0, b0;
+        private double[] an;
+        private double[] bn;
+        private double[] cn;
+        private double[] dn;
+        private int degree;
+
+        // solves the equation Laplace(u) = 0 on the unit disk, 
+        public AnnularLaplaceData(List<double> targets1, double R1, List<double> targets2, double R2, int degree)
+        {
+
+            this.an = new double[degree + 1];
+            this.bn = new double[degree + 1];
+            this.cn = new double[degree + 1];
+            this.dn = new double[degree + 1];
+            this.degree = degree;
+
+            var m1 = targets1.Average();
+            var m2 = targets2.Average();
+            this.b0 = (m1 - m2) / (Math.Log(R1) - Math.Log(R2));
+            this.a0 = ((m1 - this.b0 * Math.Log(R1)) + (m2 - this.b0 * Math.Log(R2))) / 2.0; // these two summands should be the same.
+
+            // can obviously be parallelized
+            for (int i = 1; i < degree; i++)
+            {
+                var g1i = targets1.Select((double v, int ii) => 2.0 * v * Math.Cos((double)(i * ii) / (double) targets1.Count * 2.0 * Math.PI)).Average();
+                var g2i = targets2.Select((double v, int ii) => 2.0 * v * Math.Cos((double)(i * ii) / (double) targets2.Count * 2.0 * Math.PI)).Average();
+                var det = Math.Pow(R1 / R2, i) - Math.Pow(R2 / R1, i);
+                this.an[i] = 1.0 / det * (Math.Pow(R2, -i) * g1i - Math.Pow(R1, -i) * g2i);
+                this.bn[i] = 1.0 / det * (-Math.Pow(R2, i) * g1i + Math.Pow(R1, i) * g2i);
+
+                var g1ic = targets1.Select((double v, int ii) => 2.0 * v * Math.Sin((double)(i * ii) / (double)targets1.Count * 2.0 * Math.PI)).Average();
+                var g2ic = targets2.Select((double v, int ii) => 2.0 * v * Math.Sin((double)(i * ii) / (double)targets2.Count * 2.0 * Math.PI)).Average();
+                this.cn[i] = 1.0 / det * (Math.Pow(R2, -i) * g1ic - Math.Pow(R1, -i) * g2ic);
+                this.dn[i] = 1.0 / det * (-Math.Pow(R2, i) * g1ic + Math.Pow(R1, i) * g2ic);
+            }
+        }
+
+        public double eval(Point2d p)
+        {
+            var r = Math.Sqrt(p.X * p.X + p.Y * p.Y);
+            var theta = Math.Atan2(p.Y, p.X);
+            // Horner's rule could speed it up but not bottleneck
+            return
+                this.a0 + this.b0 * Math.Log(r) +
+                Enumerable.Range(1, this.degree).Sum(i =>
+                 (Math.Pow(r, i) * this.an[i] + Math.Pow(r, -i) * this.bn[i]) * Math.Cos(i * theta) +
+                 (Math.Pow(r, i) * this.cn[i] + Math.Pow(r, -i) * this.dn[i]) * Math.Sin(i * theta)
+                 );
+        }
+    }
+
+
     public class MinSurfaceComponent : GH_Component
     {
-        int _n;
-        int _degree;
-
-        Mesh _domainMesh;
-        Mesh _outputMesh;
-        // required data
-
-        Curve _targetCurve;
-        Curve _targetCurve2;
-
-        List<Point3d> _targetPoints;
-        List<Point3d> _targetPoints2;
-
-        int _nrVert;
-
-
-        private struct LaplaceData
-        {
-            public double a0;
-            public double[] an;
-            public double[] bn;
-        }
 
         public MinSurfaceComponent()
           : base("Mini", "mini",
@@ -47,12 +104,6 @@ namespace MinSurface
         {
         }
 
-
-        /// <summary>
-        /// Each component must have a unique Guid to identify it. 
-        /// It is vital this Guid doesn't change otherwise old ghx files 
-        /// that use the old ID will partially fail during loading.
-        /// </summary>
         public override Guid ComponentGuid
         {
             get { return new Guid("6679fe76-1914-4cf2-a2da-a3b12cef0ff3"); }
@@ -80,17 +131,10 @@ namespace MinSurface
                 "The default to 1000.", GH_ParamAccess.item, 1000);
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
             => pManager.AddMeshParameter("Output mesh", "outputMesh", "The output mesh, an approximate minimal surface.", GH_ParamAccess.item);
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
+
         protected override void SolveInstance(IGH_DataAccess DA)
         {// the input curve
             Curve tc = null;
@@ -109,104 +153,76 @@ namespace MinSurface
             if (tc2 == null || !tc2.IsValid || !tc2.IsClosed) { this.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "second input curve unvalid or not closed."); return; }
 
 
-            Init(tc, tc2, k);//22ms
+            Curve _targetCurve = tc;
+            Curve _targetCurve2 = tc2;
+
+
+            // number of control points, tells about the complexity of the curve
+            int nrCont = _targetCurve.ToNurbsCurve().Points.Count;
+
+            // the degree of the curve
+            int crDeg = _targetCurve.Degree;
+
+            int idealDegree = nrCont * crDeg;
+
+            // Chosen degree of complex analytic polynomials
+            // we cap the degree to lie within 15 and 75 ... anything beyond would me meaningless
+            int _degree = Math.Min(Math.Max(15, idealDegree), 75);
+
+            //  number of boundary subdivisions
+            int _n = 23 * _degree;
+
+
+            // number of control points, tells about the complexity of the curve
+            int nrCont2 = _targetCurve2.ToNurbsCurve().Points.Count;
+
+            // the degree of the curve
+            int crDeg2 = _targetCurve2.Degree;
+
+            int idealDegree2 = nrCont2 * crDeg2;
+
+            // Chosen degree of complex analytic polynomials
+            // we cap the degree to lie within 15 and 75 ... anything beyond would me meaningless
+            int _degree2 = Math.Min(Math.Max(15, idealDegree2), 75);
+
+            //  number of boundary subdivisions
+            int _n2 = 23 * _degree2;
+
+
+
+
+
+            double[] t = _targetCurve.DivideByCount(_n, true);
+            var _targetPoints = new List<Point3d>();
+            for (int i = 0; i < _n; i++) _targetPoints.Add(_targetCurve.PointAt(t[i]));
+
+            double[] t2 = _targetCurve2.DivideByCount(_n2, true);
+            var _targetPoints2 = new List<Point3d>();
+            for (int i = 0; i < _n2; i++) _targetPoints2.Add(_targetCurve2.PointAt(t[i]));
+
             // now, do the actual work and compute the three complex polynomials
             // ok, let's get the coefficients
             List<double> xs = _targetPoints.Select(o => o.X).ToList();// 1 ms
-            LaplaceData kx = solveLaplace2(xs);
+            LaplaceData kx = new LaplaceData(xs, _degree);
             List<double> ys = _targetPoints.Select(o => o.Y).ToList();
-            LaplaceData ky = solveLaplace2(ys);
+            LaplaceData ky = new LaplaceData(ys, _degree);
             List<double> zs = _targetPoints.Select(o => o.Z).ToList();
-            LaplaceData kkz = solveLaplace2(zs);
+            LaplaceData kkz = new LaplaceData(zs, _degree);
 
-            _domainMesh = Mesh.CreateFromPlanarBoundary((new Circle(1.0)).ToNurbsCurve(), new MeshingParameters(), .001);
+            var _domainMesh = Mesh.CreateFromPlanarBoundary((new Circle(1.0)).ToNurbsCurve(), new MeshingParameters(), .001);
+            
 
             // this loop takes 170ms for 10000 vertices ... so, it is the bottleneck of the entire procedure
             // so it's worth to parallelize it
             for (int ii = 0; ii < _domainMesh.Vertices.Count; ii++)
             {
                 var p = new Point2d(_domainMesh.Vertices[ii].X, _domainMesh.Vertices[ii].Y);
-
-                double newx2 = evalPolynomial2(kx, p);
-                double newy2 = evalPolynomial2(ky, p);
-                double newz2 = evalPolynomial2(kkz, p);
-
+                double newx2 = kx.eval(p);
+                double newy2 = ky.eval(p); // evalPolynomial2(ky, p);  <-- maybe it's faster because it will be inlined?
+                double newz2 = kkz.eval(p); // evalPolynomial2(kkz, p);
                 _domainMesh.Vertices.SetVertex(ii, newx2, newy2, newz2);
             }
-
             DA.SetData(0, _domainMesh);
-
-        }
-
-
-        private double evalPolynomial2(LaplaceData k, Point2d p)
-        {
-            var r = Math.Sqrt(p.X * p.X + p.Y * p.Y);
-            var theta = Math.Atan2(p.Y, p.X);
-            return k.a0 + Enumerable.Range(1, _degree).Sum(i => Math.Pow(r, i) * (k.an[i] * Math.Cos(i * theta) + k.bn[i] * Math.Sin(i * theta)));
-        }
-
-
-        // solves the equation Laplace(u) = 0 on the unit disk, 
-        private LaplaceData solveLaplace2(List<double> targets)
-        {
-            var k = new LaplaceData();
-            k.a0 = targets.Average();
-            k.an = new double[_degree + 1];
-            k.bn = new double[_degree + 1];
-
-            // can obviously be parallelized
-            for (int i = 1; i < _degree; i++)
-            {
-                k.an[i] = targets.Select((double v, int ii) => 2.0 * v * Math.Cos((double)(i * ii) / (double)targets.Count * 2 * Math.PI)).Average();
-                k.bn[i] = targets.Select((double v, int ii) => 2.0 * v * Math.Sin((double)(i * ii) / (double)targets.Count * 2 * Math.PI)).Average();
-            }
-
-            return k;
-        }
-
-
-
-        public void Init(Curve tc, Curve tc2, int k)
-        {
-            Control.UseBestProviders();
-            
-            _targetCurve = tc;
-            _targetCurve2 = tc2;
-
-            _nrVert = k;
-
-
-            // number of control points, tells about the complexity of the curve
-            int nrCont = Math.Max(_targetCurve.ToNurbsCurve().Points.Count, _targetCurve2.ToNurbsCurve().Points.Count);
-
-            // the degree of the curve
-            int crDeg = Math.Max(_targetCurve.Degree, _targetCurve2.Degree);
-
-            int idealDegree = nrCont * crDeg;
-
-            // Chosen degree of complex analytic polynomials
-            // we cap the degree to lie within 15 and 75 ... anything beyond would me meaningless
-            _degree = Math.Min(Math.Max(15, idealDegree), 75);
-
-            //  number of boundary subdivisions
-            _n = 23 * _degree;
-
-            double[] t = _targetCurve.DivideByCount(_n, true);
-            _targetPoints = new List<Point3d>();
-            for (int i = 0; i < _n; i++)
-            {
-                _targetPoints.Add(_targetCurve.PointAt(t[i]));
-            }
-
-            double[] t2 = _targetCurve2.DivideByCount(_n, true);
-            _targetPoints2 = new List<Point3d>();
-            for (int i = 0; i < _n; i++)
-            {
-                _targetPoints2.Add(_targetCurve2.PointAt(t[i]));
-            }
-
         }
     }
-
 }
