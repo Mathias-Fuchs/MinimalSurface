149aed39f79ffee2bf82dbaa2ee11368b4e44048
Mathias Fuchs <mathias@mathiasfuchs.com>
2021-12-23 12:02:59 +0100

    yupp

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MinSurface")]
+[assembly: AssemblyDescription("compute an approximate minimal surface without recursion")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mathias Fuchs")]
+[assembly: AssemblyProduct("MinSurface.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9fc96a47-ec7f-a207-af72-0f52c43cdc70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: NeutralResourcesLanguage("en")]
